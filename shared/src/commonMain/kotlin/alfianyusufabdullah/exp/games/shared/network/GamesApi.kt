df2766c931b26099bcfb5e891d7ae444f560d10a
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-09-03 22:27:23 +0700

    added ignoreUnknownKeys to json initialization

--- shared/src/commonMain/kotlin/alfianyusufabdullah/exp/games/shared/network/GamesApi.kt
+++ shared/src/commonMain/kotlin/alfianyusufabdullah/exp/games/shared/network/GamesApi.kt
@@ -1,6 +1,6 @@
 package alfianyusufabdullah.exp.games.shared.network
 
-import alfianyusufabdullah.exp.games.shared.entity.Game
+import alfianyusufabdullah.exp.games.shared.entity.GameResponse
 import io.ktor.client.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
@@ -10,15 +10,15 @@ import kotlinx.serialization.json.Json as SerializerJson
 class GamesApi {
     private val httpClient = HttpClient {
         install(JsonFeature) {
-            val json = SerializerJson { }
+            val json = SerializerJson { ignoreUnknownKeys = true }
             serializer = KotlinxSerializer(json = json)
         }
     }
 
-    suspend fun getAllGames(): List<Game> =
+    suspend fun getAllGames(): GameResponse =
         httpClient.get(GAMES_ENDPOINT)
 
     companion object {
-        private const val GAMES_ENDPOINT = "https://api.raw.io/api/games"
+        private const val GAMES_ENDPOINT = "https://api.rawg.io/api/games"
     }
 }
