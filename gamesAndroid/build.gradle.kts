6f8721048a3eb1d4e356731459c0dddea62db5d9
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-09-03 23:09:29 +0700

    added some dependencies

--- gamesAndroid/build.gradle.kts
+++ gamesAndroid/build.gradle.kts
@@ -17,7 +17,14 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("com.google.android.material:material:1.2.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.core:core:1.3.1")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.1")
+
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+
 }
 android {
     compileSdkVersion(29)
